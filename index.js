ef3323730d30c661306f83db8eb220b43f95d040
Estefany Galvez <91927832+EstefanyGM@users.noreply.github.com>
2022-03-29 17:38:25 -0700

    MAYUSCULAS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const{mongoose} = require('./database');
 const app = express();
 
 //Puerto
-app.set('port', process.env.Port || 3000);
+app.set('port', process.env.PORT || 3000);
 
 //Middelware
 app.use(morgan('dev'));
